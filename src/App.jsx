f5e0637ffdbdfaa203fdabfd53acfe9b397b1d9b
Jaeyoung <103552850+Wodud5683@users.noreply.github.com>
2024-06-10 15:36:23 +0900

    calculator 미완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 // TODO: 계산결과를 redux를 이용한 전역상태로 관리해 보세요.
+
 function App() {
+  const [inputValue, setInputValue] = useState(0);
+  const dispatch = useDispatch();
+  const result = useSelector((state) => state.calculator.result);
+
+  const handleAdd = () => {
+    dispatch(add(Number(inputValue)));
+    setInputValue(0);
+  };
+
+  const handleSubtract = () => {
+    dispatch(subtract(Number(inputValue)));
+    setInputValue(0);
+  };
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input
+          type="nember"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        만큼을 <button>더할게요</button> <button>뺄게요</button>
       </div>
       <hr />
       <div>
--- src/redux/slices/calculatorSlice.js
+++ src/redux/slices/calculatorSlice.js
@@ -1,1 +1,25 @@
-// TODO: result 상태와 덧셈, 뺄셈에 대한 상태변경 로직을 담은 slice를 만들어 보세요.
+// TODO: store 셋업하고 slice에서 import한 reducer를 할당하세요.
+
+import { calculatorSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = calculatorSlice({
+  name: "counter",
+  initialState: {
+    result: 0,
+  },
+  reducers: {
+    increment: (state, action) => {
+      state.result += action.payload;
+    },
+    decrement: (state, action) => {
+      state.result -= action.payload;
+    },
+    reset: (state) => {
+      state.result = 0;
+    },
+  },
+});
+
+export const { increment, decrement, reset } = counterSlice.actions;
+
+export default counterSlice.reducer;
